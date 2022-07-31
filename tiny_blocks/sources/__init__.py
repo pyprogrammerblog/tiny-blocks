2ca6d40fc40941408b5d1e5786db0b88ac41f6e7
Jose <josevazjim88@gmail.com>
2022-07-31 14:26:00 +0200

    adding

--- tiny_blocks/sources/__init__.py
+++ tiny_blocks/sources/__init__.py
@@ -1,7 +1,6 @@
 from tiny_blocks.sources.csv import CSVSource
-from tiny_blocks.sources.json import JSONSource
 from tiny_blocks.sources.sql import SQLSource
 from typing import Union
 
 
-AnySource = Union[CSVSource, JSONSource, SQLSource]
+AnySource = Union[CSVSource, SQLSource]
