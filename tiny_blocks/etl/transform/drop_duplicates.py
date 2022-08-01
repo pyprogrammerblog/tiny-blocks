2ee604dfb10fafdb5f57e03a7c5914acc13030b7
Jose <josevazjim88@gmail.com>
2022-08-01 17:34:17 +0200

    adding

--- tiny_blocks/etl/transform/drop_duplicates.py
+++ tiny_blocks/etl/transform/drop_duplicates.py
@@ -47,9 +47,11 @@ class DropDuplicates(TransformBase):
 
             # select non-duplicated rows. It is also possible to select
             # a non-duplicated subset of rows.
-            sql = f"""SELECT * FROM temp WHERE rowid not in
+            sql = f"""
+            SELECT * FROM temp WHERE rowid not in
             (SELECT MIN(rowid) from temp
-            GROUP BY {", ".join(self.kwargs.subset) or "'*'"})"""
+            GROUP BY {", ".join(self.kwargs.subset) or "'*'"})
+            """
 
             # yield records now without duplicates
             chunk = self.kwargs.chunksize
