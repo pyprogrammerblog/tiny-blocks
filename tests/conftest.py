c6c4d1d662a0a6a17b73f0f2514344c1828c4e5f
Jose <josevazjim88@gmail.com>
2022-08-01 12:32:52 +0200

    adding

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import pandas as pd
 import sqlite3
 import tempfile
+from sqlalchemy import create_engine
 from tiny_blocks.sources.csv import CSVSource
 from tiny_blocks.sources.sql import SQLSource
 from tiny_blocks.sinks.csv import CSVSink
@@ -51,44 +52,72 @@ def sqlite_sink():
 
 
 @pytest.fixture
-def sql_postgres_source():
+def postgres_uri():
+    uri = "psycopg2+postgres://user:pass@postgres:5432/db?charset=utf8mb4"
+    yield uri
+
+
+@pytest.fixture
+def postgres_conn(postgres_uri):
+    engine = create_engine(postgres_uri)
+    conn = engine.connect()
+    conn.execution_options(stream_results=True)
+    try:
+        yield conn
+    finally:
+        conn.close()
+        engine.dispose()
+
+
+@pytest.fixture
+def sql_postgres_source(postgres_conn, postgres_uri):
     """
     Yield a SQL Source with a connection string to an existing Table DB
     """
-    with tempfile.NamedTemporaryFile(suffix=".db") as file, sqlite3.connect(
-        file.name
-    ) as con:
-        data = {"c": [1, 2, 3], "d": [4, 5, 6], "e": [7, 8, 9]}
-        pd.DataFrame(data=data).to_sql(name="TEST", con=con, index=False)
-        yield SQLSource(connection_string=f"sqlite:///{file.name}")
+    data = {"c": [1, 2, 3], "d": [4, 5, 6], "e": [7, 8, 9]}
+    pd.DataFrame(data=data).to_sql(name="TEST", con=postgres_conn, index=False)
+    yield SQLSource(connection_string=postgres_uri)
 
 
 @pytest.fixture
-def sql_postgres_sink():
+def sql_postgres_sink(postgres_uri):
     """
     Yield a SQL Sink with a connection string to an existing Table DB
     """
-    with tempfile.NamedTemporaryFile(suffix=".db") as file:
-        yield SQLSink(connection_string=f"sqlite:///{file.name}")
+    yield SQLSink(connection_string=postgres_uri)
 
 
 @pytest.fixture
-def sql_mysql_source():
+def mysql_uri():
+    uri = "mysql+pymysql://user:pass@mysqldb:3306/db?charset=utf8mb4"
+    yield uri
+
+
+@pytest.fixture
+def mysql_conn(mysql_uri):
+    engine = create_engine(mysql_uri)
+    conn = engine.connect()
+    conn.execution_options(stream_results=True)
+    try:
+        yield conn
+    finally:
+        conn.close()
+        engine.dispose()
+
+
+@pytest.fixture
+def sql_mysql_source(mysql_conn, mysql_uri):
     """
     Yield a SQL Source with a connection string to an existing Table DB
     """
-    with tempfile.NamedTemporaryFile(suffix=".db") as file, sqlite3.connect(
-        file.name
-    ) as con:
-        data = {"c": [1, 2, 3], "d": [4, 5, 6], "e": [7, 8, 9]}
-        pd.DataFrame(data=data).to_sql(name="TEST", con=con, index=False)
-        yield SQLSource(connection_string=f"sqlite:///{file.name}")
+    data = {"c": [1, 2, 3], "d": [4, 5, 6], "e": [7, 8, 9]}
+    pd.DataFrame(data=data).to_sql(name="TEST", con=mysql_conn, index=False)
+    yield SQLSource(connection_string=mysql_uri)
 
 
 @pytest.fixture
-def sql_mysql_sink():
+def sql_mysql_sink(mysql_uri):
     """
     Yield a SQL Sink with a connection string to an existing Table DB
     """
-    with tempfile.NamedTemporaryFile(suffix=".db") as file:
-        yield SQLSink(connection_string=f"sqlite:///{file.name}")
+    yield SQLSink(connection_string=mysql_uri)
