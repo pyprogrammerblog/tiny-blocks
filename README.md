4ec36542561fd157ceca58f966443520e367cf32
Jose <josevazjim88@gmail.com>
2022-08-02 17:23:54 +0200

    adding

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 
 Tiny Blocks to build large and complex pipelines!
 
+This module defines a library for streaming operations, composed using the `>>`
+operator. This allows for easy to read and write shell-like pipelines.
+
+### Pipeline Components: Sources, Pipes, and Sinks
+This library relies on a fundamental streaming abstraction consisting of three
+parts: sources, pipes, and sinks. You can view a pipeline as a source, followed
+by zero or more pipes, followed by a sink. Visually, this looks like:
+
+```python
+    source >> pipe1 >> pipe2 >> ... >> pipeN >> sink
+```
+
 Installation
 -------------
 
@@ -14,6 +26,8 @@ pip install tiny-blocks
 
 Basic usage example
 --------------------
+
+
 
 ```python
 from tiny_blocks.extract import ExtractCSV
--- tiny_blocks/extract/base.py
+++ tiny_blocks/extract/base.py
@@ -4,6 +4,8 @@ from typing import Iterator
 
 import pandas as pd
 from tiny_blocks.base import BaseBlock, KwargsBase
+from tiny_blocks.transform.base import TransformBase
+from tiny_blocks.load.base import LoadBase
 
 __all__ = ["ExtractBase", "KwargsExtractBase"]
 
@@ -27,3 +29,21 @@ class ExtractBase(BaseBlock):
     @abc.abstractmethod
     def get_iter(self) -> Iterator[pd.DataFrame]:
         raise NotImplementedError
+
+    def __rshift__(self, next: TransformBase | LoadBase) -> "ExtractBase":
+        """
+        The `>>` operator for the pipes library.
+        Usually this is not used, and instead the implementation on `Source`
+        is used. This will only be used if you do not start a pipe chain with a
+        `Source`, but rather with a `Pipe`.
+        A `Pipe` may be combined with another `Pipe` to form a `Pipe`.
+        A `Pipe` may be combined with a `Sink` to form a `Sink`.
+        (Strings are treated as file sinks!)
+        """
+        if isinstance(next, TransformBase):
+            next.get_iter(self.get_iter())
+            return next
+        elif isinstance(next, LoadBase):
+            next.exhaust(generator=self.get_iter())
+        else:
+            raise TypeError(f"Unexpected Block type {type(next)}")
