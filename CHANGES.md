2acbd3aaa3e1e01a78545155387ad5a8c4a8cce2
Jose Vazquez <josevazjim88@gmail.com>
2022-08-15 12:58:01 +0200

    adding...

---
+++ CHANGES.md
@@ -0,0 +1,19 @@
+Changelog
+===================================
+
+
+0.1.1 (2022-08-15)
+-------------------
+
+- Improve documentation.
+- Added new blocks.
+- Bug fixes.
+
+
+0.1.0 (2022-08-15)
+-------------------
+
+- Initial project structure.
+- Test suite.
+- Examples.
+- Documentation.
--- setup.py
+++ setup.py
@@ -1,32 +1,21 @@
 from setuptools import setup
 
-import os
 
-
-version = "0.0.1"
+version = "0.1.1"
 
 long_description = "\n\n".join(
-    [open("README.rst").read(), open("CHANGES.rst").read()]
+    [
+        open("README.md").read(),
+        open("README_USERS.md").read(),
+        open("CHANGES.md").read(),
+    ]
 )
-
-install_requires = []
-
-# emulate "--no-deps" on the readthedocs build (there is no way to specify this
-# behaviour in the .readthedocs.yml)
-if os.environ.get("READTHEDOCS") == "True":
-    install_requires = []
-
-
-tests_require = [
-    "pytest",
-]
 
 setup(
     name="tiny-blocks",
     version=version,
     description="Tiny Blocks to build large and complex pipelines!",
     long_description=long_description,
-    # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -34,15 +23,10 @@ setup(
     ],
     keywords=["python", "tiny-blocks"],
     author="JM Vazquez",
-    author_email="",
     url="https://github.com/pyprogrammerblog/tiny-blocks",
     license="MIT License",
     packages=["tiny_blocks"],
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
-    tests_require=tests_require,
     python_requires=">=3.9",
-    extras_require={"test": tests_require},
-    entry_points={"console_scripts": []},
 )
