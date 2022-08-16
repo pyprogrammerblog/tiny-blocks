a67fff9ed1a5d20d2b8ffe07fb3fd6987d13a97b
Jose Vazquez <josevazjim88@gmail.com>
2022-08-16 17:19:52 +0200

    adding...

--- tiny_blocks/pipeline.py
+++ tiny_blocks/pipeline.py
@@ -103,13 +103,13 @@ class Pipeline:
         The `>>` operator for the tiny-blocks library.
         """
         if isinstance(next, FanIn):
-            self._generators = list(next.get_iter())
+            self._generator = next.get_iter()
             return self
         elif isinstance(next, ExtractBase):
-            self._generators = list(next.get_iter())
+            self._generator = [next.get_iter()]
             return self
         elif isinstance(next, TransformBase):
-            self._generators = list(next.get_iter(*self._generators))
+            self._generator = [next.get_iter(*self._generator)]
             return self
         elif isinstance(next, LoadBase):
             next.exhaust(*self._generators)
