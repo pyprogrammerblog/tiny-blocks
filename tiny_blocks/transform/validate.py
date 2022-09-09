e9008580d5aec1dbf9a47ae1ae60df382600773a
jose.vazquez <jose.vazquez@ing.com>
2022-09-09 16:45:13 +0200

    adding

--- tiny_blocks/transform/validate.py
+++ tiny_blocks/transform/validate.py
@@ -2,7 +2,6 @@ import logging
 import pandas as pd
 import pandera as pa
 from typing import Literal, Iterator
-from pydantic import Field
 from tiny_blocks.transform.base import TransformBase
 
 __all__ = ["Validate", "SchemaErrors"]
@@ -19,17 +18,11 @@ class SchemaErrors(Exception):
     pass
 
 
-class SchemaError(Exception):
-    """
-    Raised when first error found. Lazy=False
-    """
-
-    pass
-
-
 class Validate(TransformBase):
     """
     Validate block. Defines block to apply validation.
+
+    When `lazy=True` all errors are collected and raised at the end.
 
     Basic example:
         >>> import pandas as pd
@@ -37,9 +30,7 @@ class Validate(TransformBase):
         >>> from tiny_blocks.extract import FromCSV
         >>>
         >>> from_csv = FromCSV(path='/path/to/file.csv')
-        >>> validate = Validate(
-        ...   schema_model=my_schema_validation, lazy=True
-        >>> )
+        >>> validate = Validate(schema_model=pandera_schema, lazy=True)
         >>>
         >>> generator = from_csv.get_iter()
         >>> generator = validate.get_iter(generator)
@@ -48,11 +39,7 @@ class Validate(TransformBase):
 
     name: Literal["validate"] = "validate"
     schema_model: pa.SchemaModel
-    lazy: bool = Field(
-        default=True,
-        description="When set to True, collect all "
-        "errors and raise them at the end",
-    )
+    lazy: bool = True
 
     def get_iter(
         self, source: Iterator[pd.DataFrame]
@@ -62,8 +49,7 @@ class Validate(TransformBase):
 
         for chunk in source:
             try:
-                self.schema_model.validate(chunk, lazy=self.lazy)
-                yield chunk
+                yield self.schema_model.validate(chunk, lazy=self.lazy)
             except pa.errors.SchemaErrors as errs:
                 collector = pd.concat([collector, errs.failure_cases])
 
